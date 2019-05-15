89eac4bf9b5dc9b9c930989079885a169c4ed37b
Max M <meshstyles@users.noreply.github.com>
2019-05-15 20:43:31 +0200

    sweeping the area

--- milestone_1_max/main.md
+++
@@ -1,5 +0,0 @@
-# Main
-this is the main class of the project
-
-## why isn't there a releas yet if you got the code working?
-welp I can't compile the Project.java for some reason
--- oldstuff/kb/cal.java
+++ oldstuff/kb/cal.java
@@ -11,16 +11,23 @@ public class cal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String iString = sc.nextLine();
+        String iString2 = sc.nextLine();
         sc.close();
         SimpleDateFormat format = new SimpleDateFormat("yyyy/mm/dd");
         Date date2 = new Date();
+        Date date1 = new Date();
         try {
-            date2 = format.parse(iString);            
+            date2 = format.parse(iString);
+            date1 = format.parse(iString2);
         } catch ( ParseException e) {
+            System.out.println("meep");
             //ignore
         }
         SimpleDateFormat format2 = new SimpleDateFormat("dd-mm-yy");
         String datePrint = format2.format(date2);
+        
+        System.out.println(date2.after(date1));
+        
         System.out.println(datePrint);
 
     }
